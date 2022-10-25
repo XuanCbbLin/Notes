e090b641da494e0eabeb86294b94609dcb6a5113
xuan <xuancbblin@gmail.com>
2022-10-25 18:35:43 +0800

    刪除部屬設定

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  base: '/Notes/',
   themeConfig: {
     nav: [
       { text: 'Vue', link: '/vue/Creating-Vue-Application' },
