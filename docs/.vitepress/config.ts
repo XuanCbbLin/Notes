0fcf8bc8e26e4a76ae743f2084818aa5cda0420d
xuan <xuancbblin@gmail.com>
2022-10-25 18:23:59 +0800

    chore : set deploying

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/Notes/',
   themeConfig: {
     nav: [
       { text: 'Vue', link: '/vue/Creating-Vue-Application' },
